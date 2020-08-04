f283c29508bdaab4f15223ce2cfa60ad1c3a3576
Anushree Bagchi <anushreebagchi4791@gmail.com>
2020-08-04 10:45:40 +0530

    Order.js added

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {formatPrice} from '../helpers';
 
 class Order extends React.Component {
     render(){
+        const {fishes, order} = this.props;
+        const orderIds = Object.keys(order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const price = fishes[key].price;
+            const count = order[key];
+            if(fishes[key].status == "available"){
+                return prevTotal + price*count;
+            }
+            return prevTotal;
+        },0);
+
         return (
-            <div className="order">
-                
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <ul>
+                    {orderIds}
+                </ul>
+                <div> Total: {formatPrice(total)}</div>
             </div>
             )
     }
